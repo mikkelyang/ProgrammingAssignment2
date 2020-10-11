0bb2903c7d07b0102a925b823d199fcc524c7f3a
mikkelyang <mikkelyang@gmail.com>
2020-10-11 18:54:04 +0800

    Created the function: makeCacheMatrix and cacheSolver for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat <- x
+    inv <- NA
+    get <- function () mat
+    set_inverse <- function (inverse) inv <<- inverse
+    get_inverse <- function () inv
+    list(get=get, set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
 
@@ -12,4 +17,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$get_inverse()
+    if(!is.na(inv)){
+            print('Getting from Cache')
+            return(inv)
+    }
+    data <- x$get()
+    comp_inv <- solve(data)
+    x$set_inverse(comp_inv)
+    comp_inv
 }
